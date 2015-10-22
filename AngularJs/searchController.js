93d9e7ae1e67c8fd7f4182e4c7283e7cfe650bd1
Cláudio Ribeiro <claudio.ribeiro@gatewit.com>
2015-10-22 15:39:04 +0100

    API usage

---
+++ AngularJs/searchController.js
@@ -0,0 +1,37 @@
+'use strict';
+
+angular.module('searchApp', [])
+  .controller('searchController', function($scope, $http){
+    var pendingTask;
+
+    if($scope.search === undefined){
+      $scope.search = "Entrepreneur";
+      fetch();
+    }
+
+	$scope.change = function(){
+      if(pendingTask){
+        clearTimeout(pendingTask);
+      }
+      pendingTask = setTimeout(fetch, 800);
+    };
+
+    function fetch(){
+      /*$http.get("http://www.omdbapi.com/?t=" + $scope.search + "&tomatoes=true&plot=full")
+       .success(function(response){ $scope.details = response; });*/
+	   
+	   $http.get("http://www.reddit.com/r/entrepreneur/search.json?q=" + $scope.search + "&limit=2")
+       .success(function(response){ $scope.results = response; });
+    }
+
+	//Change this
+    $scope.update = function(movie){
+      $scope.search = movie.Title;
+      $scope.change();
+    };
+
+    $scope.select = function(){
+      this.setSelectionRange(0, this.value.length);
+    }	
+	
+  });
